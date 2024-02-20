d442f1b49dd515ab9d5c8ad90876f64de343fec9
lgalarzaLoteria <lgalarza@loteria.com.ec>
2024-02-20 11:26:54 -0500

    Para incorporar el uso de DEVEXPRESS

--- IntranetLoteriaNacional/Program.cs
+++ IntranetLoteriaNacional/Program.cs
@@ -7,6 +7,7 @@ using Syncfusion.Blazor;
 using Microsoft.AspNetCore.Http.Features;
 using IntranetLoteriaNacional.Shared.Constants;
 using CurrieTechnologies.Razor.SweetAlert2;
+using DevExpress.Blazor;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,6 +19,7 @@ builder.Services.AddBlazoredSessionStorage();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSweetAlert2();
 builder.Services.AddSingleton<StoreCheckService>();
+builder.Services.AddDevExpressBlazor(configure => configure.BootstrapVersion = BootstrapVersion.v5);
 
 /***Servicio para peticiones livianas***/
 if (!builder.Services.Any(x => x.ServiceType == typeof(HttpClient)))
@@ -45,6 +47,7 @@ Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt+QHFq
 builder.WebHost.UseStaticWebAssets();
 Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBaFt+QHFqVkNrXVNbdV5dVGpAd0N3RGlcdlR1fUUmHVdTRHRcQllhS3xRdkVhXnxedHY=;Mgo+DSMBPh8sVXJ1S0d+X1RPd11dXmJWd1p/THNYflR1fV9DaUwxOX1dQl9gSXpRdEdhWX9cd3dUQWI=;ORg4AjUWIQA/Gnt2VFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5XdEdhW39ecnNWRGle;MTgxMTQyNEAzMjMxMmUzMTJlMzMzNWgvRjVXcmVGam1sMDlQZ01kdzE2Sm5MTFRQWEo5NUoyaTdnRTlJMEJxMWs9;MTgxMTQyNUAzMjMxMmUzMTJlMzMzNUhyOXIyeEVzbXpTUjY2TWQvY2xHZjF3RFBOakNSMXRXT3kvWjRsSExoUk09;NRAiBiAaIQQuGjN/V0d+XU9Hc1RDX3xKf0x/TGpQb19xflBPallYVBYiSV9jS31TckdhW31ecHZSRWlbUw==;MTgxMTQyN0AzMjMxMmUzMTJlMzMzNUJYT09TblNDTHJNaytRWDY1eW5ZVExVT0pwWGhOMk9vblBxT05VTVN1Q3M9;MTgxMTQyOEAzMjMxMmUzMTJlMzMzNU41VkxXalQ1ZW5CZjhlZXgwN0huM1VJQm45dkVLdFdncXN0UjNSS1U1VWc9;Mgo+DSMBMAY9C3t2VFhhQlJBfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5XdEdhW39ecnNRQGle;MTgxMTQzMEAzMjMxMmUzMTJlMzMzNUtYYzJxS2h0MDE0V1FCcmJ6OGdJUms2VnduZFJRWlpwYUtyRzFBVGpiZFU9;MTgxMTQzMUAzMjMxMmUzMTJlMzMzNWVMZzVZQnRhR0xDNE0yUzdhcDJFUkU1OFh0bXhjNjdwNExxM1hyRFozdmc9;MTgxMTQzMkAzMjMxMmUzMTJlMzMzNUJYT09TblNDTHJNaytRWDY1eW5ZVExVT0pwWGhOMk9vblBxT05VTVN1Q3M9");
 
+builder.WebHost.UseWebRoot("wwwroot");
 builder.WebHost.UseStaticWebAssets();
 
 
